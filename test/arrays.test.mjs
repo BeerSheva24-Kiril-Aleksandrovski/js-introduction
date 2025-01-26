315f8cbc5dc017d8a70718c6eb8940bfef7b6fc4
Kiril <fanky.yahoo@gmail.com>
2025-01-26 15:58:03 +0200

    ADD: sorting

--- test/arrays.test.mjs
+++ test/arrays.test.mjs
@@ -60,3 +60,12 @@ describe("iterating elements of array", () => {
         ar.forEach((e, i) => console.log(`index: ${i}; element: ${e}`))
     })
 })
+
+describe("sorting array", () => {
+    const array = [10, 1000, -10, 30, 60]
+    it("numbers native sorting soting", () => {
+        const expected = [-10, 10, 30, 60, 1000];
+        expect(array.toSorted((a, b) => a - b)).toEqual(expected);
+    })
+
+})
