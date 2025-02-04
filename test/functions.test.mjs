0819a1fd380710902dea000dee488efe75140aa8
Kiril <fanky.yahoo@gmail.com>
2025-02-04 21:33:20 +0200

    ADD: myBind implementation used Symbol()

--- test/functions.test.mjs
+++ test/functions.test.mjs
@@ -1,23 +1,23 @@
-import {describe, it, expect} from 'vitest';
+import { describe, it, expect } from 'vitest';
 import { myBind } from '../functions.mjs';
 describe("myBind test according to the TODO comments", () => {
-    const point = {x:3, y:4}; 
-    const dimensions = {x:3, y:4, z:30};
+    const point = { x: 3, y: 4 };
+    const dimensions = { x: 3, y: 4, z: 30, [Symbol()]: 30 };
     point.sumArguments
-    function sumArguments(num3=0, num4=0) {
+    function sumArguments(num3 = 0, num4 = 0) {
         return this.x + this.y + num3 + num4;
     }
     sumArguments.bind = myBind;
-    sumArguments.toString = function() {return this.name}
+    sumArguments.toString = function () { return this.name }
     console.log(`sumArguments string: ${sumArguments}`)
     const fun = sumArguments.bind(dimensions);
     it("no additional parameters passed", () => {
-        
+
         expect(fun()).toBe(7);
     })
     it(" all parameters are passed inside fun", () => {
         expect(fun(10, 20)).toBe(37)
-    })
+    });
     it(" all parameters are passed inside bind along with this argument", () => {
         const fun = sumArguments.bind(dimensions, 10, 20);
         expect(fun()).toBe(37)
